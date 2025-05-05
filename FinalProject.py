e95c0cff36a795b2647ca6dc71bd26c6295a4854
Cottier-Mary <mcottier@nevada.unr.edu>
2025-05-05 14:19:17 -0700

    updated project file
    trying some different funcitonalities

--- FinalProject.py
+++ FinalProject.py
@@ -7,87 +7,102 @@ from mavros_msgs.msg import OverrideRCIn
 
 class FinalProject:
     def __init__(self):
-        # info variables
+        #variables for storing information
         self.apriltag_data = None
         self.apriltag_detection = False
 
-        # nodes & services
+        #creating nodes and services
         rospy.init_node('final_project_node', anonymous=True)
         rospy.wait_for_service('/minihawk_SIM/mavros/set_mode')
         rospy.wait_for_service('/minihawk_SIM/mavros/cmd/arming')
 
-        # service calls
-        self.set_mode = rospy.ServiceProxy('/minihawk_SIM/mavros/set_mode', SetMode)
-        self.arm = rospy.ServiceProxy('/minihawk_SIM/mavros/cmd/arming', CommandBool)
+        # call services
+        self.mode_service = rospy.ServiceProxy('/minihawk_SIM/mavros/set_mode', SetMode)
+        self.arm_service = rospy.ServiceProxy('/minihawk_SIM/mavros/cmd/arming', CommandBool)
 
-        try:
-            self.set_mode(0, 'AUTO')
-            self.arm(True)
-        except rospy.ServiceException as e:
-            rospy.logerr(f'Service call failed: {e}')
+        self.set_mode('AUTO')
+        self.arm_motors()
 
-        rospy.Subscriber('/minihawk_SIM/MH_usb_camera_link_optical/tag_detections', AprilTagDetectionArray, self.apriltag_callback)
-        while not self.apriltag_detection and not rospy.is_shutdown():
-            rospy.sleep(0.1)
-        rospy.sleep(3)
+        rospy.Subscriber('/minihawk_SIM/MH_usb_camera_link_optical/tag_detections',
+                         AprilTagDetectionArray, self.apriltag_callback)
 
+        self.wait_for_apriltag()
         self.finetune_position()
+        self.set_mode('QLAND')
 
+    def set_mode(self, mode_name):
         try:
-            self.set_mode(0, 'QLAND')
+            self.mode_service(0, mode_name)
         except rospy.ServiceException as e:
-            rospy.logerr(f'Service call failed: {e}')
+            print('Errored while setting mode:', e)
+
+    def arm_motors(self):
+        try:
+            self.arm_service(True)
+        except rospy.ServiceException as e:
+            print('Errored while arming:', e)
 
     def apriltag_callback(self, msg):
         if msg.detections:
             self.apriltag_data = msg.detections[0]
             self.apriltag_detection = True
 
-    # sets to qloiiter
-    def finetune_position(self):
-        try:
-            self.set_mode(0, 'QLOITER')
-        except rospy.ServiceException as e:
-            rospy.logerr(f'Service call failed: {e}')
+    def get_apriltag_position(self, pose):
+        while hasattr(pose, "pose"):
+            pose = pose.pose
+        return pose.position
 
+    def wait_for_apriltag(self):
+        while not self.apriltag_detection and not rospy.is_shutdown():
+            rospy.sleep(0.1)
+        rospy.sleep(3)
+
+    # set to qloiter
+    def finetune_position(self):
+        self.set_mode('QLOITER')
         pub = rospy.Publisher('/minihawk_SIM/mavros/rc/override', OverrideRCIn, queue_size=10)
-        Kp, Ki, Kd = 10.0, 0.1, 5.0 # pid variables
+
+        # PID constants
+        Kp, Ki, Kd = 10.0, 0.1, 5.0
         ix = iy = ex_prev = ey_prev = 0.0
         last_time = rospy.Time.now().to_sec()
 
         # position adjustments
         while not rospy.is_shutdown():
-            if not self.apriltag_data: # take data
+            if not self.apriltag_data:
+                # take data
                 continue
 
-            pos = self.apriltag_data.pose.pose.position
+            pos = self.get_apriltag_position(self.apriltag_data.pose)
             dx, dy = pos.x, pos.y
-            rospy.loginfo((dx, dy))
+            print(dx, dy)
 
             if abs(dx) < 1 and abs(dy) < 1:
+                print('it tried to break')
                 break
 
             now = rospy.Time.now().to_sec()
             dt = max(now - last_time, 1e-3)
             last_time = now
 
-            # roll
+            # Roll (Y axis control)
             iy += dy * dt
             dy_d = (dy - ey_prev) / dt
             roll = int(max(1000, min(2000, 1500 + Kp * dy + Ki * iy + Kd * dy_d)))
             ey_prev = dy
 
-            # pitch
+            # Pitch (X axis control)
             ix += dx * dt
             dx_d = (dx - ex_prev) / dt
             pitch = int(max(1000, min(2000, 1500 + Kp * dx + Ki * ix + Kd * dx_d)))
             ex_prev = dx
 
             control = OverrideRCIn()
-            control.channels = [roll, pitch, 1500, 1500] + [0]*14
+            control.channels = [roll, pitch, 1500, 1500] + [0] * 14
             pub.publish(control)
 
             rospy.sleep(0.5)
+
 
 if __name__ == '__main__':
     FinalProject()
